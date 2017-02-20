92b346bf3357589640f7c7ec78adfde5ba421b8f
salsada <alberto_botero@hotmail.com>
2017-02-20 16:11:18 -0500

    20170120

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/public/home.html');
+});
+
+module.exports = app;
