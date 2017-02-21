a097eaf46bd36465538d3e86d0c433edd8611779
Bill Gray <wgray01@wgray.com>
2017-02-20 21:02:40 -0500

    1

---
+++ public/js/exercise1.js
@@ -0,0 +1,41 @@
+function findLargest() {
+
+    //Get Values//
+    var number1 = document.getElementById('number1').value;
+    var number2 = document.getElementById('number2').value;
+    var number3 = document.getElementById('number3').value;
+
+    //Validate Values//
+    if (isNaN(number1) || number1.length == 0) {
+        alert('Number 1 is not a number, or is empty!!');
+        return;
+    }
+    if (isNaN(number2) || number2.length == 0) {
+        alert('Number 2 is not a number, or is empty!!');
+        return;
+    }
+    if (isNaN(number3) || number3.length == 0) {
+        alert('Number 3 is not a number, or is empty!!');
+        return;
+    }
+    //Convert to an actual number//
+    var n1 = Number(number1);
+    var n2 = Number(number2);
+    var n3 = Number(number3);
+
+    //Find Largest Value//
+    if (n1 > n2) {
+        if (n2 > n3) {
+            alert('The largest number is ' + n1);
+        } else {
+            alert('The largest number is ' + n3);
+        }
+    } else {
+        if (n2 > n3) {
+            alert('The largest number is ' + n2);
+        } else {
+            alert('The largest number is ' + n3);
+        }
+    }
+
+}
