4bd38d259a500814581422d7825216077e86e57b
Bill Gray <wgray01@wgray.com>
2017-03-01 20:24:34 -0500

    1

--- public/js/angular1.js
+++ public/js/angular1.js
@@ -1,6 +1,19 @@
 var app = angular.module('app', []);
 
+app.controller('headCtrl', function($scope) {
+    $scope.TitleH1 = 'HTML5, CSS3, and Javascript'
+    $scope.TitleSmall = ' Programming'
+});
+
 app.controller('recipesCtrl', function($scope) {
     $scope.Name = 'Welcome to Angular';
     $scope.Recipes = ['Fried Chicken', 'T-Bone Steak', 'Dirty Rice'];
+});
+
+app.controller('recipesCtrl1', function($scope) {
+    $scope.Recipe = {
+        name: "Fried Chicken",
+        description: "How to make fried chicken",
+        steps: ["Step 1", "Step 2", "Step 3"]
+    };
 });
